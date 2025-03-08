3931fb02fb0c0ca240a61d8b4d56315fe2e4ea3b
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-08 23:19:28 +0100

    signup fragment navigation buttons

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/SignupFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/SignupFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
@@ -21,5 +22,13 @@ class SignupFragment : Fragment() {
 		// Inflate the layout for this fragment
 		binding = FragmentSignupBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		binding.signup.setOnClickListener {
+			findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+		}
 	}
 }
