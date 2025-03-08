1213eb3b084fa0604998c2c5c1270f1f36ca8d06
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-08 23:37:37 +0100

    scaffold fragment navigation buttons

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ScaffoldFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ScaffoldFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentScaffoldBinding
 
 class ScaffoldFragment : Fragment() {
@@ -21,5 +23,23 @@ class ScaffoldFragment : Fragment() {
 		// Inflate the layout for this fragment
 		binding = FragmentScaffoldBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		binding.logout.setOnClickListener {
+			findNavController().navigate(R.id.action_scaffoldFragment_to_loginFragment)
+		}
+		
+		binding.list.setOnClickListener {
+			binding.fragmentContainerView.findNavController().navigate(R.id.listFragment)
+		}
+		binding.favorites.setOnClickListener {
+			binding.fragmentContainerView.findNavController().navigate(R.id.favoritesFragment)
+		}
+		binding.contact.setOnClickListener {
+			binding.fragmentContainerView.findNavController().navigate(R.id.contactFragment)
+		}
 	}
 }
