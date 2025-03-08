e2fa57e664f09b5edcc22edd783863db95be33d4
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-08 23:19:20 +0100

    login fragment navigation buttons

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/LoginFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -21,5 +22,16 @@ class LoginFragment : Fragment() {
 		// Inflate the layout for this fragment
 		binding = FragmentLoginBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		binding.login.setOnClickListener {
+			findNavController().navigate(R.id.action_loginFragment_to_scaffoldFragment)
+		}
+		binding.signup.setOnClickListener {
+			findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+		}
 	}
 }
