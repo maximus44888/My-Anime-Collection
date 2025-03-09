63857cab7df8c094a90d6cca2df800405cde7b06
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-09 03:07:00 +0100

    updated contact fragment ids

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ContactFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ContactFragment.kt
@@ -1,18 +1,14 @@
 package pujalte.martinez.juan.projectosegundaevaluacion
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
 	private lateinit var binding: FragmentContactBinding
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-	}
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
