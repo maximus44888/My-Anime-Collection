ab0c1510604e59abd266e6909174cfed154b74ca
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-09 23:00:24 +0100

    updated favorites view

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/FavoritesFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/FavoritesFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import pujalte.martinez.juan.projectosegundaevaluacion.adapters.ItemAdapter
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
@@ -17,5 +20,23 @@ class FavoritesFragment : Fragment() {
 		// Inflate the layout for this fragment
 		binding = FragmentFavoritesBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		initialize(savedInstanceState)
+	}
+	
+	private fun initialize(saveInstanceState: Bundle?) {
+		val viewModel = ViewModelProvider(requireActivity())[ScaffoldViewModel::class.java]
+		val adapter = ItemAdapter(listOf())
+		
+		binding.rv.layoutManager = LinearLayoutManager(requireContext())
+		binding.rv.adapter = adapter
+		
+		viewModel.data.observe(viewLifecycleOwner) {
+			adapter.updateItems(it.filter { it.isFavorite })
+		}
 	}
 }
