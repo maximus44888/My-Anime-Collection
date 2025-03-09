72d28c1f1f1aa8c29c55f4b7b5a4599e575bb908
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-09 03:02:53 +0100

    updated signup fragment ids

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/SignupFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/SignupFragment.kt
@@ -1,19 +1,15 @@
 package pujalte.martinez.juan.projectosegundaevaluacion
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
 	private lateinit var binding: FragmentSignupBinding
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-	}
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +23,7 @@ class SignupFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		
-		binding.signup.setOnClickListener {
+		binding.fragmentSignupSignupButton.setOnClickListener {
 			findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
 		}
 	}
