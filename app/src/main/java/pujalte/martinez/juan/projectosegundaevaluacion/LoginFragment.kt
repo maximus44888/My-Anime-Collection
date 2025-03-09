f6a64981da725a9a27ffe5c4db89b4121ee5b216
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-09 18:40:49 +0100

    updated login view

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/LoginFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/LoginFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -23,11 +27,57 @@ class LoginFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		
-		binding.fragmentLoginLoginButton.setOnClickListener {
-			findNavController().navigate(R.id.action_loginFragment_to_scaffoldFragment)
-		}
-		binding.fragmentLoginSignupButton.setOnClickListener {
-			findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
-		}
+		initialize(savedInstanceState)
 	}
+	
+	private fun initialize(savedInstanceState: Bundle?) {
+		fun updateLoginSignupIsEnabled() {
+			val isEnabled =
+				!binding.userInput.text.isNullOrBlank() && !binding.passwordInput.text.isNullOrBlank()
+			binding.loginButton.isEnabled = isEnabled
+			binding.signupButton.isEnabled = isEnabled
+		}
+		
+		fun setupTextInputLayoutError(
+			textInputLayout: TextInputLayout, error: String = getString(R.string.cant_be_empty),
+		) {
+			textInputLayout.editText?.doAfterTextChanged {
+				if (it.isNullOrBlank()) {
+					textInputLayout.error = error
+					textInputLayout.isErrorEnabled = true
+					updateLoginSignupIsEnabled()
+				} else {
+					textInputLayout.error = null
+					textInputLayout.isErrorEnabled = false
+					updateLoginSignupIsEnabled()
+				}
+			}
+		}
+		
+		setupTextInputLayoutError(binding.userInputLayout)
+		setupTextInputLayoutError(binding.passwordInputLayout)
+		
+		fun setupButtonSnackbar(
+			button: Button,
+			snackbarText: CharSequence = button.text,
+			duration: Int = Snackbar.LENGTH_SHORT,
+			actionText: CharSequence? = null,
+			listener: View.OnClickListener = View.OnClickListener {},
+		) {
+			button.setOnClickListener {
+				Snackbar.make(binding.root, snackbarText, duration).setAction(actionText, listener)
+					.show()
+			}
+		}
+		
+		binding.loginButton.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_scaffoldFragment) }
+		binding.signupButton.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signupFragment) }
+		
+//		setupButtonSnackbar(binding.loginButton)
+//		setupButtonSnackbar(binding.signupButton)
+		setupButtonSnackbar(binding.forgotPasswordButton)
+		setupButtonSnackbar(binding.googleLoginButton)
+		setupButtonSnackbar(binding.facebookLoginButton)
+	}
+	
 }
