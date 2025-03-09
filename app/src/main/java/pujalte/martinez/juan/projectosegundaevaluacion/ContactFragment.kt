82e2769215ad372f01b5b159bf4ef06e1c84dea4
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-09 18:58:28 +0100

    updated contact view

--- app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ContactFragment.kt
+++ app/src/main/java/pujalte/martinez/juan/projectosegundaevaluacion/ContactFragment.kt
@@ -1,10 +1,18 @@
 package pujalte.martinez.juan.projectosegundaevaluacion
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import pujalte.martinez.juan.projectosegundaevaluacion.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -18,4 +26,126 @@ class ContactFragment : Fragment() {
 		binding = FragmentContactBinding.inflate(inflater, container, false)
 		return binding.root
 	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		initialize(savedInstanceState)
+	}
+	
+	private fun initialize(savedInstanceState: Bundle?) {
+		fun setupClickableToIntentWithPermissions(
+			clickable: View,
+			editText: EditText,
+			permission: String?,
+			intentSupplier: (clickable: EditText) -> Intent,
+			rationalePermission: String = getString(R.string.default_rationale_permission),
+			rationaleUnavailability: String = getString(R.string.default_rationale_unavailability),
+			unableToResolveActivityMessage: String = getString(R.string.default_unable_to_resolve_activity_message),
+		) {
+			fun runIntent() {
+				val intent = intentSupplier(editText)
+				if (intent.resolveActivity(requireContext().packageManager) != null) {
+					startActivity(intent)
+				} else {
+					Snackbar.make(
+						binding.root, unableToResolveActivityMessage, Snackbar.LENGTH_LONG
+					).show()
+				}
+			}
+			
+			val onClickListener = if (permission == null) {
+				View.OnClickListener {
+					runIntent()
+				}
+			} else {
+				val requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+					if (it) {
+						runIntent()
+					} else {
+						Snackbar.make(
+							binding.root, rationaleUnavailability, Snackbar.LENGTH_LONG
+						).show()
+					}
+				}
+				View.OnClickListener {
+					when {
+						ContextCompat.checkSelfPermission(
+							requireContext(), permission
+						) == PERMISSION_GRANTED -> {
+							runIntent()
+						}
+						
+						shouldShowRequestPermissionRationale(permission) -> {
+							Snackbar.make(
+								binding.root, rationalePermission, Snackbar.LENGTH_LONG
+							).show()
+						}
+						
+						else -> {
+							requestPermissionLauncher.launch(permission)
+						}
+					}
+					
+				}
+			}
+			
+			clickable.setOnClickListener(onClickListener)
+		}
+		
+		setupClickableToIntentWithPermissions(
+			binding.whatsappButton,
+			binding.phone,
+			null,
+			{ editText ->
+				Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/${editText.text}")).apply {
+					setPackage("com.whatsapp")
+				}
+			},
+			unableToResolveActivityMessage = getString(R.string.whatsapp_unable_to_resolve_activity_message)
+		)
+		setupClickableToIntentWithPermissions(
+			binding.phoneButton,
+			binding.phone,
+			Manifest.permission.CALL_PHONE,
+			{ editText ->
+				Intent(Intent.ACTION_CALL, Uri.parse("tel:${editText.text}"))
+			},
+			getString(R.string.phone_rationale_permission),
+			getString(R.string.phone_rationale_unavailability),
+			getString(R.string.phone_unable_to_resolve_activity_message)
+		)
+		setupClickableToIntentWithPermissions(
+			binding.emailButton,
+			binding.email,
+			null,
+			{ editText ->
+				Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:${editText.text}"))
+			},
+			unableToResolveActivityMessage = getString(R.string.email_unable_to_resolve_activity_message)
+		)
+		setupClickableToIntentWithPermissions(
+			binding.locationButton,
+			binding.location,
+			Manifest.permission.ACCESS_COARSE_LOCATION,
+			{ editText ->
+				Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=${editText.text}"))
+			},
+			getString(R.string.location_rationale_permission),
+			getString(R.string.location_rationale_unavailability),
+			getString(R.string.location_unable_to_resolve_activity_message)
+		)
+		setupClickableToIntentWithPermissions(
+			binding.map,
+			binding.location,
+			Manifest.permission.ACCESS_COARSE_LOCATION,
+			{ editText ->
+				Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=${editText.text}"))
+			},
+			getString(R.string.location_rationale_permission),
+			getString(R.string.location_rationale_unavailability),
+			getString(R.string.location_unable_to_resolve_activity_message)
+		)
+	}
+	
 }
