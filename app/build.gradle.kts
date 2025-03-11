6814b90cf4fe0cd7994a71dd16be098f860ee716
Juan Pujalte Martínez <56847436+maximus44888@users.noreply.github.com>
2025-03-11 15:23:52 +0100

    firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -44,6 +45,11 @@ dependencies {
 	
 	implementation(libs.androidx.navigation.fragment.ktx)
 	implementation(libs.androidx.navigation.ui.ktx)
+	
+	implementation(libs.firebase.auth)
+	implementation(libs.androidx.credentials)
+	implementation(libs.androidx.credentials.play.services.auth)
+	implementation(libs.googleid)
 	
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.appcompat)
